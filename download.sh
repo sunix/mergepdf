8f727cb7cb3bed0b5c763688f58826f422e94a3c
Sun Seng David TAN <sutan@redhat.com>
2020-05-28 18:24:14 +0200

    cp download

--- download.sh
+++ download.sh
@@ -2,3 +2,4 @@
 mkdir $chap; cd $chap; wget https://www.scan-vf.net/uploads/manga/one_piece/chapters/chapitre-$chap/{01..50}.jpg;
 for jpgfile in $(ls *.jpg); do convert $jpgfile  "$jpgfile".pdf; done
 pdftk *.pdf cat output large.pdf && pdf2ps large.pdf very_large.ps && ps2pdf very_large.ps final.pdf && rm large.pdf very_large.ps
+cp final.pdf ../onepiece-chapitre-"$chap".pdf
