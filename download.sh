e31e1ef39953a0d82d6f8a53526d8bc1c0dd02be
Sun Seng David TAN <sutan@redhat.com>
2020-05-29 15:37:00 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-mkdir $chap; cd $chap; wget https://www.scan-vf.net/uploads/manga/one_piece/chapters/chapitre-$chap/{01..50}.jpg;
-for jpgfile in $(ls *.jpg); do convert $jpgfile  "$jpgfile".pdf; done
+mkdir $chap; cd $chap; wget https://www.scan-vf.net/uploads/manga/one_piece/chapters/chapitre-$chap/{01..25}.png; wget https://www.scan-vf.net/uploads/manga/one_piece/chapters/chapitre-$chap/{01..25}.jpg;
+for jpgfile in $(ls *.*g); do convert $jpgfile  "$jpgfile".pdf; done
 pdftk *.pdf cat output large.pdf && pdf2ps large.pdf very_large.ps && ps2pdf very_large.ps final.pdf && rm large.pdf very_large.ps
-cp final.pdf ../onepiece-chapitre-"$chap".pdf
+
+# could be executed with:
+# for chap in {0..199}; do /download.sh ; cp $chap/final.pdf onepiece-chapitre-"$(printf "%03d" $chap)".pdf; rm $chap -rf; done
