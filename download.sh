946085c71f417332303952939b754ac851ffd673
Sun Seng David TAN <sutan@redhat.com>
2020-06-16 21:59:29 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-mkdir $chap; cd $chap; wget https://www.scan-vf.net/uploads/manga/one_piece/chapters/chapitre-$chap/{01..25}.png; wget https://www.scan-vf.net/uploads/manga/one_piece/chapters/chapitre-$chap/{01..25}.jpg;
-for jpgfile in $(ls *.*g); do convert $jpgfile  "$jpgfile".pdf; done
+mkdir $chap; cd $chap;
+wget https://www.scan-vf.net/uploads/manga/one_piece/chapters/chapitre-$chap/%20\({1..9}\).png;
+wget https://www.scan-vf.net/uploads/manga/one_piece/chapters/chapitre-$chap/%20\({1..9}\).jpg;
+rename.ul '(' '0' \ *
+wget https://www.scan-vf.net/uploads/manga/one_piece/chapters/chapitre-$chap/%20\({10..25}\).png;
+wget https://www.scan-vf.net/uploads/manga/one_piece/chapters/chapitre-$chap/{01..25}.png;
+wget https://www.scan-vf.net/uploads/manga/one_piece/chapters/chapitre-$chap/%20\({10..25}\).jpg;
+wget https://www.scan-vf.net/uploads/manga/one_piece/chapters/chapitre-$chap/{01..25}.jpg;
+wget https://www.scan-vf.net/uploads/manga/one_piece/chapters/chapitre-$chap/{01..25}.webp;
+rename.ul '(' '' \ *
+rename.ul ')' '' \ *
+rename.ul ' ' '' \ *
+for jpgfile in $(ls *.*g; ls *.webp); do convert "$jpgfile"  "$jpgfile".pdf; done
 pdftk *.pdf cat output large.pdf && pdf2ps large.pdf very_large.ps && ps2pdf very_large.ps final.pdf && rm large.pdf very_large.ps
 
 # could be executed with:
