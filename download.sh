5befcdfa94d5c7a9d231756ef85c8d265e8928f4
Sun Seng David TAN <sutan@redhat.com>
2020-09-05 09:45:29 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir $chap; cd $chap;
-wget http://lelscano.com/mangas/one-piece/$chap/{01..25}.jpg;
+wget https://lelscan.net/mangas/one-piece/$chap/{01..25}.jpg;
 for jpgfile in $(ls *.*g); do convert "$jpgfile"  "$jpgfile".pdf; done
 pdftk *.pdf cat output large.pdf && pdf2ps large.pdf very_large.ps && ps2pdf very_large.ps final.pdf && rm large.pdf very_large.ps
 
