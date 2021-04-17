2c2da32716399463b09fd3746d82630ff295ed5b
Sun Seng David TAN <sutan@redhat.com>
2021-04-18 00:13:10 +0200

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,11 +11,9 @@ if [ -w "${HOME}" ] && [ ! -f "${HOME}"/.bashrc ]; then
 fi
 
 # Add current (arbitrary) user to /etc/passwd and /etc/group
-if ! whoami &> /dev/null; then
-  if [ -w /etc/passwd ]; then
-    echo "${USER_NAME:-user}:x:$(id -u):0:${USER_NAME:-user} user:${HOME}:/bin/bash" >> /etc/passwd
-    echo "${USER_NAME:-user}:x:$(id -u):" >> /etc/group
-  fi
+if [ -w /etc/passwd ]; then
+  echo "${USER_NAME:-user}:x:$(id -u):0:${USER_NAME:-user} user:${HOME}:/bin/bash" >> /etc/passwd
+  echo "${USER_NAME:-user}:x:$(id -u):" >> /etc/group
 fi
 
 exec "$@"
