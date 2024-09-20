8a47f72bb44ecd5d64894b8cdc6579bba6a5b18f
Sun S D Tan <sunix@sunix.org>
2024-09-20 13:22:18 +0200

    update
    Signed-off-by: Sun S D Tan <sunix@sunix.org>

--- download.sh
+++ download.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-chap=1032
+chap=
+url=
+titleprefix=
+# extract from url what is before "p1.svgz"
+urlprefix=$(echo $url | sed 's/\(.*\)p1.svgz.*/\1/')
+# extract from url what is after "p1.svgz"
+urlsuffix=$(echo $url | sed 's/.*p1.svgz\(.*\)/\1/')
+
 mkdir -p $chap
 cd $chap;
-wget https://p.calameoassets.com/$id/p{0..25}.svgz;
-rename -e 's/\d+/sprintf("%02d",$&)/e' -- *.svgz
-cp ../onepiece-blank-page.svgz p01bis.svgz;
+# download the pages
+for i in {1..25};
+do
+  wget -O p$i.svgz ${urlprefix}p$i.svgz${urlsuffix};
+done
+
+cp ../p1bis.svgz .;
 
 for svgzfile in $(ls *.svgz);
 do
@@ -12,19 +23,37 @@ do
   convert "$svgzfile"  "$svgzfile".pdf;
 done
 
-pdfjam p01bis.svgz.pdf p01.svgz.pdf \
-       p03.svgz.pdf p02.svgz.pdf \
-       p05.svgz.pdf p04.svgz.pdf \
-       p07.svgz.pdf p06.svgz.pdf \
-       p09.svgz.pdf p08.svgz.pdf \
-       p11.svgz.pdf p10.svgz.pdf \
-       p13.svgz.pdf p12.svgz.pdf \
-       p15.svgz.pdf p14.svgz.pdf \
-       p17.svgz.pdf p16.svgz.pdf \
-       --nup 2x1 --landscape --outfile final.pdf
-       #p19.svgz.pdf p18.svgz.pdf \
-       #p21.svgz.pdf p20.svgz.pdf \
+# dynamically generate the folowing command with the existing pages in a string
+# pdfjam p1bis.svgz.pdf p1.svgz.pdf \
+#       p3.svgz.pdf p2.svgz.pdf \
+#       p5.svgz.pdf p4.svgz.pdf \
+#       p7.svgz.pdf p6.svgz.pdf \
+#       p9.svgz.pdf p8.svgz.pdf \
+#       p11.svgz.pdf p10.svgz.pdf \
+#       p13.svgz.pdf p12.svgz.pdf \
+#       --nup 2x1 --landscape --outfile final.pdf
+
+
+command="pdfjam p1bis.svgz.pdf p1.svgz.pdf";
+# if double cover page it should be this command instead:
+# command="pdfjam ";
+
+# for each odd page, add the even page before it
+for i in {2..25};
+do
+  if [ $((i%2)) -eq 0 ]; then
+    #if file exists
+    if [ -f p$(($i+1)).svgz.pdf ]; then
+      command="$command p$(($i+1)).svgz.pdf p$i.svgz.pdf";
+    fi
+  fi
+done
+command="$command --nup 2x1 --landscape --outfile final.pdf"
+# display the command
+echo $command;
+# execute the command
+$command;
 
 cd ../;
-cp $chap/final.pdf onepiece-chapitre-"$(printf "%03d" $chap)".pdf;
+cp $chap/final.pdf $titleprefix-"$(printf "%03d" $chap)".pdf;
 rm $chap -rf;
